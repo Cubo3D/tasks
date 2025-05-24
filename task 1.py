ff5ee033afd1632fb3aef73f0cee5f749be50ade
cubo3d <davisilvaperes3@gmail.com>
2025-05-24 15:52:57 -0300

    tasks 1 completa

---
+++ task 1.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+print("Aperte 'ctrl + c' para parar")
+print("ou se não você verá essas mensagens HAHAHHAHA")
+sleep(2)
+while True:
+    print("Aku sayang kamu juga sayang tapi sayang")
