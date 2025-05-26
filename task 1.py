abb4876f5481df6b47a150ad91d45d01d7081179
cubo3d <davisilvaperes3@gmail.com>
2025-05-26 12:58:42 -0300

    correção do código

--- task 1.py
+++ task 1.py
@@ -1,11 +1,7 @@
 from time import sleep
 
-if input("qual é a senha? ") == "q":
-    print("senha correta. saindo...")
-    sleep(1)
-    exit()
-else:
-    print("senha errada")
-    sleep(2)
-    while True:
-        print("Aku sayang kamu juga sayang tapi sayang")
+while True:
+    if input("qual é a senha? ") == "q":
+        print("senha correta. saindo...")
+        sleep(1)
+        exit()
